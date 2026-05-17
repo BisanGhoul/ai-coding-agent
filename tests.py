6da82e17783d8d6c35fb1a174e2bec344233508f
Bisan Ghoul <bisan.elghoul@gmail.com>
2026-05-17 17:43:19 +0300

    feat: add write_file tool with path validation and directory creation

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def test_get_file_info():
     print("Result for current directory:")
@@ -25,6 +26,11 @@ def test_get_file_content():
     print(get_file_content("calculator", "/bin/cat"))
     print(get_file_content("calculator", "pkg/does_not_exist.py"))
 
+def test_write_file():
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
 
 # test_get_file_info();
-test_get_file_content();
+#test_get_file_content();
+test_write_file()
