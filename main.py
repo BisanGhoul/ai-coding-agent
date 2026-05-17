e1edbb54976f414ec22b3201ba60775b52497743
Bisan Ghoul <bisan.elghoul@gmail.com>
2026-05-17 16:45:19 +0300

    feat: add CLI chatbot with Gemini API integration

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [
+        types.Content(
+            role="user",
+            parts=[types.Part(text=args.user_prompt)]
+        )
+    ]
+
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=messages
+    )
+    if response is None or response.usage_metadata is None:
+        print("No response or usage metadata received.")
+        return
+    print(response.text)
+
+    if args.verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+main()
