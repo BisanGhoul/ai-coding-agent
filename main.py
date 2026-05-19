46d26a85179eca83c5b08cf1de9d7e7a3e31a71e
Bisan Ghoul <bisan.elghoul@gmail.com>
2026-05-19 19:02:46 +0300

    feat: add function declarations and tool config for LLM function calling

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt 
+from call_function import available_functions
 
 def main():
     load_dotenv()
@@ -26,14 +27,17 @@ def main():
         model='gemini-2.5-flash',
         contents=messages,
         config=types.GenerateContentConfig(
+            tools=[available_functions],
             system_instruction=system_prompt,
             temperature=0
         ),
     )
-    if response is None or response.usage_metadata is None:
-        print("No response or usage metadata received.")
-        return
-    print(response.text)
+    
+    if response.function_calls:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
 
     if args.verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
